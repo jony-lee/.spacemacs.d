1f7e6ad62ae69b79351313566cd4b051cd78beac
jony <jonylee.cn@gmail.com>
2020-04-07 10:23:42 +0800

    change startup-banner

--- init.el
+++ init.el
@@ -179,7 +179,8 @@ It should only modify the values of Spacemacs settings."
    ;; directory. A string value must be a path to an image format supported
    ;; by your Emacs build.
    ;; If the value is nil then no banner is displayed. (default 'official)
-   dotspacemacs-startup-banner 'official
+   ;; dotspacemacs-startup-banner 'official
+   dotspacemacs-startup-banner 0
 
    ;; List of items to show in startup buffer or an association list of
    ;; the form `(list-type . list-size)`. If nil then it is disabled.
@@ -226,8 +227,8 @@ It should only modify the values of Spacemacs settings."
    dotspacemacs-colorize-cursor-according-to-state t
 
    ;; Default font or prioritized list of fonts.
-   dotspacemacs-default-font '(;;"Source Code Pro"
-                               "Consolas"
+   dotspacemacs-default-font '("Source Code Pro"
+                               ;;"Consolas"
                                :size 12.0
                                :weight normal
                                :width normal)
