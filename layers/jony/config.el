cec93375aee573353eeb322d59611eb0dabdf028
lichong <lichong_i@didichuxing.com>
2020-09-15 17:43:36 +0800

    禁用剪贴板共享功能

--- layers/jony/config.el
+++ layers/jony/config.el
@@ -31,18 +31,18 @@
 					))
 	)
 ;; mac和emacs共享剪贴板
-(defun copy-from-osx ()
-  (shell-command-to-string "pbpaste"))
+;; (defun copy-from-osx ()
+;;   (shell-command-to-string "pbpaste"))
 
-(defun paste-to-osx (text &optional push)
-  (let ((process-connection-type nil))
-    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-      (process-send-string proc text)
-      (process-send-eof proc))))
+;; (defun paste-to-osx (text &optional push)
+;;   (let ((process-connection-type nil))
+;;     (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+;;       (process-send-string proc text)
+;;       (process-send-eof proc))))
 
-(when (string= system-type "darwin")
-  (setq interprogram-cut-function 'paste-to-osx)
-  (setq interprogram-paste-function 'copy-from-osx))
+;; (when (string= system-type "darwin")
+;;   (setq interprogram-cut-function 'paste-to-osx)
+;;   (setq interprogram-paste-function 'copy-from-osx))
 
 (when (eq system-type 'darwin)
   (require 'ls-lisp)
